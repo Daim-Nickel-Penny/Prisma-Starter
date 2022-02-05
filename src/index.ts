5d6ac4ff5a657dd36d70a2c2370a979a95d66832
1804449@kiit.ac.in <1804449@kiit.ac.in>
2022-02-05 12:50:54 +0530

    Seeded db with values for Query Test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,42 @@
 import express from "express";
-
+import { PrismaClient } from "@prisma/client";
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/", (_req, res) => {
-  res.send("Hello");
+app.use(express.json());
+
+app.get("/", async (_req, res) => {
+  const users = await prisma.user.findMany();
+  res.json({ users });
+});
+
+app.post("/user", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+      },
+    });
+    res.json({ user });
+  } catch (err) {
+    console.log(err);
+    res.json({ error: err.message });
+  }
+});
+
+app.post("/post", async (req, res) => {
+  const { authorId, title, content } = req.body;
+
+  const post = await prisma.post.create({
+    data: {
+      title,
+      content,
+      authorId: parseInt(authorId),
+    },
+  });
+  res.json({ post });
 });
 
 const port = 5000;
